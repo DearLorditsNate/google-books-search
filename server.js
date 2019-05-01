2f1c0f3969f9d56beee51adad65b8c017617b94a
Nate Micinski <nmicinski@gmail.com>
2019-05-01 14:37:21 -0700

    add front-end API routes

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const routes = require("./routes");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,9 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+// Add routes, both API and view
+app.use(routes);
 
 // Send every request to the React app
 // Define any API routes before this runs

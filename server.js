ff55177393e5614c9ab8f8551bfddb5e8275aa5b
Nate Micinski <nmicinski@gmail.com>
2019-05-01 14:32:42 -0700

    add api routes and book controller

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -13,6 +17,9 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books-search");
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);

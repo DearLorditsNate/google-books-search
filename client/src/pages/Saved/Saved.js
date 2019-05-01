9a78ff6922fc1e004320fb14297861bed43af185
Nate Micinski <nmicinski@gmail.com>
2019-05-01 16:15:26 -0700

    adds db seeds and saved books now render

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,17 +1,55 @@
 import React, { Component } from "react";
 import Jumbotron from "../../components/Jumbotron";
-// import API from "../../utils/API";
+import API from "../../utils/API";
 
 class Saved extends Component {
-    state = {
+  state = {
+    books: []
+  };
 
-    }
+  componentDidMount() {
+    console.log(this.state);
+    this.loadBooks();
+  }
 
-    render() {
-        return (
-            <Jumbotron />
-        );
-    }
+  loadBooks = () => {
+    API.getBooks()
+      .then(res => this.setState({ books: res.data }))
+      .then(console.log(this.state))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <Jumbotron />
+        {/* Saved Articles */}
+        {this.state.books.map(book => (
+          <div className="container">
+            <div className="card m-3">
+              <div className="card-header">
+                <a href={book.link} target="_blank" className="title">
+                  {book.title}
+                </a>
+                <a
+                  href="/save"
+                  className="btn btn-success save-btn"
+                  data-id="{{_id}}"
+                >
+                  Save Article
+                </a>
+              </div>
+              <div className="card-body">
+                <p className="card-text">{book.description}</p>
+                <p className="card-text">{book.authors}</p>
+                <img src={book.image}></img>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Saved;
--- models/book.js
+++ models/book.js
@@ -3,11 +3,11 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: {
-    type: [String],
+    type: String,
     required: true
   },
   authors: {
-    type: String,
+    type: [String],
     required: true
   },
   description: {
